c2da40b09343b3534f3b0a4df99223430e0c628e
staging <root@staging.thinx.cloud>
2019-06-12 00:58:48 +0200

    entrypoint starting service and then mosquitto as user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-incrontab --reload
+#exec "$@"
 
-exec "$@"
+# must be run as root
+service incron start
+#/usr/sbin/incrond -f /etc/incron.conf --foreground &&
+
+su - mosquitto
+
+/usr/sbin/mosquitto -c /mqtt/config/mosquitto.conf
+
